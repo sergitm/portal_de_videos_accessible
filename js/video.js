397df85123ece42491c7800f4778c80cf263b6b7
Sergi Triado <55805318+striado@users.noreply.github.com>
2023-03-06 02:17:38 +0100

    actu

---
+++ js/video.js
@@ -0,0 +1,7 @@
+"use strict";
+function init(){
+    $(document).ready( function() {
+        $('#navegacio').load("navbar.html");
+    });
+    $('logo').attr('src', '../assets/img/IAM_CAT_logos_transparent_rgb_2.png');
+}
