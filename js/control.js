11cf6606bd8f43a5819361fb7f678a0f75cc7152
striado <s.triado@sapalomera.cat>
2023-03-07 18:59:42 +0100

    nomes queda accessibilitat

--- js/control.js
+++ js/control.js
@@ -2,6 +2,15 @@
 
 function init(){
     $(document).ready( function() {
-        $('#navegacio').load("public/html/navbar.html");
-    })
+        $('#navegacio').load("public/html/layouts/navbar.html");
+
+        $('#videos').load('public/html/layouts/llista.html', function() {
+            $('#video1').attr('src','public/assets/videos/Aprenent anglès ballant.mp4#t=15');
+            $('#enllaç1').attr('href','public/html/video.html?v=0');
+            $('#video2').attr('src','public/assets/videos/Space Invaders.mp4#t=10');
+            $('#enllaç2').attr('href','public/html/video.html?v=1');
+            $('#video3').attr('src','public/assets/videos/treball per projectes.mp4#t=12');
+            $('#enllaç3').attr('href','public/html/video.html?v=2');
+        });
+    });
 }
--- js/video.js
+++ js/video.js
@@ -1,7 +1,64 @@
 "use strict";
 function init(){
     $(document).ready( function() {
-        $('#navegacio').load("navbar.html");
+        $('#navegacio').load('layouts/navbar.html', function() {
+            $('#logo').attr('src', '../assets/img/IAM_CAT_logos_transparent_rgb_2.png');
+            $('#enllaç-home').attr('href', '../../');
+        
+            $("#enllaç_pral").attr("href", '../../');
+            $("#enllaç_video").attr("href", './video.html');   
+
+            $("#enllaç_pral").removeClass("active");
+            $("#enllaç_video").addClass("active");
+        });
+
+        const params = new Proxy(new URLSearchParams(window.location.search), {
+            get: (searchParams, prop) => searchParams.get(prop),
+        });
+        
+        let value = params.v;
+
+        if (parseInt(value) >= 0 && parseInt(value) <= 2) {
+            loadVideo(value);
+        } else {
+            loadLlista();
+        }
     });
-    $('logo').attr('src', '../assets/img/IAM_CAT_logos_transparent_rgb_2.png');
+}
+
+function loadVideo(value){
+    $('#videos').load('layouts/video.html', function() {
+        switch (parseInt(value)) {
+            case 0:
+                $('#titol').html('Aprenent anglès ballant');
+                $('#video').attr('src', '../assets/videos/Aprenent anglès ballant.mp4');
+                break;
+            case 1:
+                $('#titol').append('Space Invaders');
+                $('#video').attr('src', '../assets/videos/Space Invaders.mp4');
+                break;
+            case 2:
+                $('#titol').append('Treball per projectes');
+                $('#video').attr('src', '../assets/videos/treball per projectes.mp4');
+                break;
+            default:
+                loadLlista();
+        }
+        $('#tornar').on('click', tornar);
+    });
+}
+
+function loadLlista(){
+    $('#videos').load('layouts/llista.html', function() {
+        $('#video1').attr('src','../assets/videos/Aprenent anglès ballant.mp4#t=15');
+        $('#enllaç1').attr('href','./video.html?v=0');
+        $('#video2').attr('src','../assets/videos/Space Invaders.mp4#t=10');
+        $('#enllaç2').attr('href','./video.html?v=1');
+        $('#video3').attr('src','../assets/videos/treball per projectes.mp4#t=12');
+        $('#enllaç3').attr('href','./video.html?v=2');
+    });
+}
+
+function tornar(){
+    window.location.href = "../../";
 }
